0e662b23e1e3364a685549e7834152c37d0eac48
Iain <iain@zora.co>
2022-07-14 18:56:29 -0400

    write urlencode lib

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,36 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import {UriEncode} from "../src/UriEncode.sol";
 
-contract ContractTest is Test {
+contract TestUriEncode is Test {
     function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    function testEncode() public {
+        assertEq(
+            UriEncode.uriEncode("encode me +.-/balj^%"),
+            "encode%20me%20%2b.-%2fbalj%5e%25"
+        );
+    }
+
+    function testBlank() public {
+        assertEq(UriEncode.uriEncode(""), "");
+    }
+
+    function testEveryEncodeChar() public {
+        assertEq(UriEncode.uriEncode("       .   "), "%20%20%20%20%20%20%20.%20%20%20");
+    }
+
+    function testNoEncodeAnyChar() public {
+        assertEq(
+            UriEncode.uriEncode(
+                "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz0123456789"
+            ),
+            "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz0123456789"
+        );
+    }
+
+    function testString(string memory input) public {
+        UriEncode.uriEncode(input);
     }
 }
